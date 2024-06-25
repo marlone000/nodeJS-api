936c6277033604f0f874a81801587edd0feeabc3
marlone000 <142364606+marlone000@users.noreply.github.com>
2024-06-24 21:03:09 -0300

    listagem de heroes

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require ('express')
 const app = express()
 
-app.get("/", function (req, res){
-    res.send("hello world")
+const herois = ["Miranha", "Verdrugo", "MisterS marvel"]
+
+
+
+// endpoint listagem de todos os herois (READ ALL => [GET])
+app.get("/heroes", function (req, res){
+    res.send(herois)
 })
 
-app.get("/ptbr", function (req, res){
-    res.send("Olá mundo")
+
+app.get("/heroes/:id", function (req, res){
+    // acessar o parametro rota id
+    const id = req.params.id
+
+    const umheroi = herois[id]
+
+    res.send(umheroi)
 })
 
-app.post()
-app.put()
-app.delete()
+// app.post()
+// app.put()
+// app.delete()
 
 
 
